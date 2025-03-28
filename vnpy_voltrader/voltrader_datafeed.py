93d68bdad704ce136ebfc6a7873714ac93da462e
noranhe <63988546+noranhe@users.noreply.github.com>
2025-03-28 10:21:33 +0800

    [Mod] 分钟线查询改成逐日查询

--- vnpy_voltrader/voltrader_datafeed.py
+++ vnpy_voltrader/voltrader_datafeed.py
@@ -106,24 +106,51 @@ class VoltraderDatafeed(BaseDatafeed):
         # 获取时间戳平移幅度
         adjustment: timedelta = INTERVAL_ADJUSTMENT_MAP[req.interval]
 
-        # 发起K线查询
-        quote_history: list[dict] = self.api.getquotehistory(
-            ice_interval,
-            ice_window,
-            name,
-            req.start.strftime("%Y%m%d%H"),
-            req.end.strftime("%Y%m%d%H")
-        )
+        # 逐日查询K线数据
+        all_quote_history: list[dict] = []
+        query_start: datetime = req.start
+
+        while query_start < req.end:
+            if req.interval in [Interval.DAILY, Interval.HOUR]:
+                quote_history: list[dict] = self.api.getquotehistory(
+                    ice_interval,
+                    ice_window,
+                    name,
+                    req.start.strftime("%Y%m%d%H"),
+                    req.end.strftime("%Y%m%d%H")
+                )
+
+                if quote_history:
+                    all_quote_history.extend(quote_history)
+                break
+
+            query_end: datetime = query_start + timedelta(days=1)
+
+            # 发起K线查询
+            quote_history: list[dict] = self.api.getquotehistory(
+                ice_interval,
+                ice_window,
+                name,
+                query_start.strftime("%Y%m%d%H"),
+                query_end.strftime("%Y%m%d%H")
+            )
+
+            # 保存查询结果
+            if quote_history:
+                all_quote_history.extend(quote_history)
+
+            # 更新查询起始时间
+            query_start = query_end
 
         # 失败则直接返回
-        if not quote_history:
+        if not all_quote_history:
             output(f"获取{req.symbol}合约{req.start}-{req.end}历史数据失败")
             return []
 
         # 转换数据格式
         bars: list[BarData] = []
 
-        for history in quote_history:
+        for history in all_quote_history:
             dt: datetime = (history["DateTime"] - adjustment).replace(tzinfo=CHINA_TZ)
             if req.interval == Interval.DAILY:
                 dt = dt.replace(hour=0, minute=0)
